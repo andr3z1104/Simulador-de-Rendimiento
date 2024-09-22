f5a4359e8e78db79c22df9cb57ab40023b375307
Franco <franco.lattanzio@correo.unimet.edu.ve>
2024-09-22 18:19:38 -0400

    crear clase trabajador

--- Simulador/src/clases/trabajador.java
+++ Simulador/src/clases/trabajador.java
@@ -3,5 +3,76 @@ package clases;
 
 
 public class trabajador {
+    private int id;
+    private String rol;
+    private int salarioPorHora;
+    private int dineroAcumulado;
+    private int produccionPorDia;
+    private int diasParaGenerarProducto;
+    private boolean activo;
+
+    // Constructor
+    public trabajador(int id, String rol, int salarioPorHora, int produccionPorDia, int diasParaGenerarProducto, boolean activo) {
+        this.id = id;
+        this.rol = rol;
+        this.salarioPorHora = salarioPorHora;
+        this.dineroAcumulado = 0;
+        this.produccionPorDia = produccionPorDia;
+        this.diasParaGenerarProducto = diasParaGenerarProducto;
+        this.activo = activo;
+    }
+
+    // Getters y Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public String getRol() {
+        return rol;
+    }
     
+    public void setRol(String Rol) {
+        this.rol = Rol;
+    }
+
+    public double getSalarioPorHora() {
+        return salarioPorHora;
+    }
+
+    public void setSalarioPorHora(int salarioPorHora) {
+        this.salarioPorHora = salarioPorHora;
+    }
+
+    public int getDineroAcumulado() {
+        return dineroAcumulado;
+    }
+
+    public void setDineroAcumulado(int dineroAcumulado) {
+        this.dineroAcumulado = dineroAcumulado;
+    }
+
+    public int getProduccionPorDia() {
+        return produccionPorDia;
+    }
+
+    public void setProduccionPorDia(int produccionPorDia) {
+        this.produccionPorDia = produccionPorDia;
+    }
+
+    public int getDiasParaGenerarProducto() {
+        return diasParaGenerarProducto;
+    }
+
+    public void setDiasParaGenerarProducto(int diasParaGenerarProducto) {
+        this.diasParaGenerarProducto = diasParaGenerarProducto;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
 }
