c7d206acb341b32e2fb2773ec5e0d717ef6875b9
Franco <franco.lattanzio@correo.unimet.edu.ve>
2024-10-01 21:43:10 -0400

    leer txt listo pero sacar sus cosas del main

--- Simulador/src/main/Main.java
+++ Simulador/src/main/Main.java
@@ -4,8 +4,12 @@ import clases.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-        
+        public static void main(String[] args) {
+            Txt claaase = new Txt();
+            String aeo = claaase.Seleccionar();
+            claaase.cargarArchivo(aeo);
+            claaase.leerArchivo();
+            System.out.println(claaase.deadline);
     }
     
 }
